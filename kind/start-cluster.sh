2061ecc1e090e0f25314e8aaa4099c8874aae71a
ujjwal-D11 <ujjwal.bagrania@dream11.com>
2025-09-23 16:08:06 +0530

    added jetstack repo

--- kind/start-cluster.sh
+++ kind/start-cluster.sh
@@ -24,6 +24,8 @@ else
     --kubeconfig "${KUBECONFIG}"
 
   # Install cert-manager with CRDs
+  helm repo add jetstack https://charts.jetstack.io
+  helm repo update
   helm install cert-manager jetstack/cert-manager \
     --namespace cert-manager \
     --create-namespace \
