29ac182e25fe6e4690f6ef20631bea890828e86f
Harsh Agarwal <110374365+HarshAgarwal11@users.noreply.github.com>
2025-11-27 20:26:13 +0530

    Fixed dcm build.sh file (#57)

--- darwin-cluster-manager/.odin/darwin-cluster-manager/build.sh
+++ darwin-cluster-manager/.odin/darwin-cluster-manager/build.sh
@@ -2,7 +2,17 @@
 
 set -e # For enabling Exit on error
 
-cp -rf ./* ./target/darwin-cluster-manager/
+cp -rf ./app ./target/darwin-cluster-manager/app
+cp -rf ./charts ./target/darwin-cluster-manager/charts
+cp -rf ./constants ./target/darwin-cluster-manager/constants
+cp -rf ./dto ./target/darwin-cluster-manager/dto
+cp -rf ./logger ./target/darwin-cluster-manager/logger
+cp -rf ./rest ./target/darwin-cluster-manager/rest
+cp -rf ./services ./target/darwin-cluster-manager/services
+cp -rf ./utils ./target/darwin-cluster-manager/utils
+cp -rf ./go.mod ./target/darwin-cluster-manager/go.mod
+cp -rf ./go.sum ./target/darwin-cluster-manager/go.sum
+cp -rf ./main.go ./target/darwin-cluster-manager/main.go
 
 # Copy the kind kubeconfig file to the target directory
 mkdir -p ./target/darwin-cluster-manager/configs
